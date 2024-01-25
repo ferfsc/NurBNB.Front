7c6ac86880b1a4e0408e199e8d330db29fd942af
ferfsc <ferfsc@gmail.com>
2024-01-25 19:23:35 -0400

    Pagina de inicio

---
+++ NurBnb.Front.Web/Login.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NurBnb.Front.Web.Controles;
+using NurBnbFront.Infrastructure;
+using NurBnb.Front.Web.AppCode;
+
+namespace NurBnb.Front.Web
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        private MensajeAlertas mensajeAlertas = new MensajeAlertas();
+        private static log4net.ILog log { get; set; } = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType); // Es lo mismo q poner type(Login)
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (!Page.IsPostBack)
+                {
+                    VaciarDatos();
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                lblMensajeError.Text = ex.Message;
+                lblMensajeError.Focus();
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+                mensajeAlertas.ErrorGenerico();
+                lblMensajeError.Text = mensajeAlertas.MensajeError;
+            }
+            finally
+            {
+                Utils.CloseLoading(this);
+                
+            }
+        }
+        private void VaciarDatos()
+        {
+            txtUsuario.Text = string.Empty;
+            txtUsuario.Focus();
+            txtPassword.Text = string.Empty;
+        }
+
+        private void ValidarDatos()
+        {
+            if (string.IsNullOrEmpty(txtUsuario.Text.Trim()))
+                throw new ArgumentException("Ingrese su Usuario");
+
+            if (string.IsNullOrEmpty(txtPassword.Text.Trim()))
+                throw new ArgumentException("Ingrese su Clave");
+
+        }
+
+        protected void btnContinuarLogin_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                ValidarDatos();
+                NurBnbFront.Infrastructure.InicioLogin objLogin = new NurBnbFront.Infrastructure.InicioLogin();
+                objLogin.UsrLogin = txtUsuario.Text;                
+                objLogin.UsrPassword = txtPassword.Text.Trim();
+
+                objLogin = objLogin.InicioSession();
+
+                if (objLogin.esUsuario)
+                {
+                    Sesion.Login = objLogin;
+                    Response.Redirect("frmPrincipal.aspx", false);
+                }
+                else
+                    lblMensajeError.Text = "Ingreso invalido.";
+            }
+            catch (ArgumentException ex1)
+            {
+                log.Error(ex1.Message, ex1);
+                lblMensajeError.Text = ex1.Message;
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+                lblMensajeError.Text = ex.Message;
+            }
+            finally
+            {
+                Utils.CloseLoading(this);
+            }
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            VaciarDatos();
+            Response.Redirect("~/Login.aspx");
+        }
+    }
+}
---
+++ NurBnb.Front.Web/Login.aspx.designer.cs
@@ -0,0 +1,89 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace NurBnb.Front.Web
+{
+
+
+    public partial class Login
+    {
+
+        /// <summary>
+        /// Control hfdispositivo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hfdispositivo;
+
+        /// <summary>
+        /// Control lblUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUsuario;
+
+        /// <summary>
+        /// Control txtUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUsuario;
+
+        /// <summary>
+        /// Control lblContrasena.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblContrasena;
+
+        /// <summary>
+        /// Control txtPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+
+        /// <summary>
+        /// Control lblMensajeError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMensajeError;
+
+        /// <summary>
+        /// Control btnContinuarLogin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnContinuarLogin;
+
+        /// <summary>
+        /// Control btnCancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCancelar;
+    }
+}
